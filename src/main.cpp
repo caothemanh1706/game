8ca5d68322b06272d14fde1b6ba8578291e1a0be
Cao Thế Mạnh <manhcao47383@gmail.com>
2025-03-05 15:26:13 +0700

    struct

--- src/main.cpp
+++ src/main.cpp
@@ -4,3 +4,22 @@ using namespace std;
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
+// dinh nghia vuot 
+const int PADDLE_WIDTH = 10;
+const int PADDLE_HEIGHT = 100;
+const int PADDLE_SPEED = 5;
+
+// dinh nghia qua bong
+const int BALL_SIZE = 10;
+const int BALL_SPEED = 4;
+
+struct Paddle {
+    int x, y;
+    int dy; 
+};
+
+struct Ball {
+    int x, y;
+    int dx, dy;
+};
+
