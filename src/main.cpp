b7c23418bf4d1d29ea3b1a421558f245f57a0c7f
Cao Thế Mạnh <manhcao47383@gmail.com>
2025-03-05 18:02:15 +0700

    vẽ bóng và 2 vợt lên trên của sổ

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ const int BALL_SPEED = 4;
 
 struct Paddle {
     int x, y;
-    int dy; 
+    int dy;
 };
 
 struct Ball {
@@ -29,9 +29,37 @@ SDL_Window* window = nullptr;
 SDL_Renderer* renderer = nullptr;
 bool running = true;
 
-void intiGame() {  // thiet lap vi tri ban dau cua vot va bong
+void initGame() {  // thiet lap vi tri ban dau cua vot va bong
     leftPaddle = { 20, (SCREEN_HEIGHT - PADDLE_HEIGHT) / 2, 0 };
-    rightPaddle = { 20 , (SCREEN_HEIGHT - PADDLE_HEIGHT) / 2,0 };
+    rightPaddle = { SCREEN_WIDTH - 30, (SCREEN_HEIGHT - PADDLE_HEIGHT) / 2,0 };
     ball = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, BALL_SPEED, BALL_SPEED };
 }
 
+int SDL_main(int argc, char* argv[]) {
+
+    SDL_Window* window = SDL_CreateWindow("Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
+    initGame();
+
+    while (running) {
+        SDL_SetRenderDrawColor(renderer, 0, 55, 55, 255); //màu nền xanh thẩm
+        SDL_RenderClear(renderer);
+
+       
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Màu trắng
+        SDL_Rect leftPaddleRect = { leftPaddle.x, leftPaddle.y, PADDLE_WIDTH, PADDLE_HEIGHT };
+        SDL_RenderFillRect(renderer, &leftPaddleRect);
+
+        SDL_Rect rightPaddleRect = { rightPaddle.x, rightPaddle.y, PADDLE_WIDTH, PADDLE_HEIGHT };
+        SDL_RenderFillRect(renderer, &rightPaddleRect);
+        SDL_Rect ballRect = { ball.x,ball.y,BALL_SIZE,BALL_SIZE };
+        SDL_RenderFillRect(renderer, &ballRect);
+
+       SDL_RenderPresent(renderer); 
+    }
+
+
+    return 0;
+
+}
