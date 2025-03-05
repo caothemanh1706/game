05ab89da6927ca9bc396818c01000aec353457d5
Cao Thế Mạnh <manhcao47383@gmail.com>
2025-03-05 14:29:23 +0700

    con

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,6 @@
 ﻿#include <SDL2/SDL.h>
 #include <iostream>
+using namespace std;
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
 
-int SDL_main(int argv, char* argc[]) {
-	std::cout << "Hello" << '\n';
-
-	SDL_Window* window = SDL_CreateWindow("Game", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-	bool running = true;
-
-	while (running) {
-		SDL_Event event;
-
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT) {
-				running = false;
-			}
-		}
-	}
-
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-
-	return 0;
-}
