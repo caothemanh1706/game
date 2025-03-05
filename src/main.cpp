a7151117a84b1a3d96b1e8c0c5b53fae7c5cf0a9
Cao Thế Mạnh <manhcao47383@gmail.com>
2025-03-05 13:37:26 +0700

    init

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+﻿#include <SDL2/SDL.h>
+
+using namespace std;
+
+int SDL_main(int argv, char* argc[]) {
+	SDL_Window* window = SDL_CreateWindow("Game", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	bool running = true;
+
+	while (running) {
+		SDL_Event event;
+
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_QUIT) {
+				running = false;
+			}
+		}
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+
+	return 0;
+}
